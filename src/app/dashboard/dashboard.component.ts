7b1c5e25f6d3e4756cb45f42b69e8374101e1346
Kottakotavenkatakeerthika <140802803+Kottakotavenkatakeerthika@users.noreply.github.com>
2023-08-08 12:59:48 +0000

    Add files via upload

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+   arrname=[
+    {name:'madhu',value:'teacher'},
+  {name:'keerthika',value:'student'},
+  {name:'ramya',value:'student2'}
+]
+}
+
